a7808f920f84fbb8977384c099c0899e9d12c4e5
joao.santos <joao.santos@lactec.org.br>
2019-12-09 18:32:04 -0300

    Melhorada a manipulação e ordenação dos blobs

--- main.py
+++ main.py
@@ -4,73 +4,74 @@ import dft as dft
 import connectedcomponents as cc
 from matplotlib import pyplot as plt
 import os
+import operator
 
 #find characters using contour
-def findCharactersUsingContours(binary_image):
-    cnts, hier = cv2.findContours(binary_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # characters = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[1][1], reverse=False)
-    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    characters, boundingBoxes = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b:b[1][1], reverse=True))
-	# (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b:b[1][0], reverse=reverse))
-    return characters
+def findCharactersUsingContours(binary_image): # Encontrar cada caracteres utilizando identificacao de blobs por FindContours
+    #cv2.imshow("a", binary_image)
+    cnts, hier = cv2.findContours(binary_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # cnts = list of contours
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts] # bounding Box of each character
+    characters = cnts
+    #print(characters)
+    boundingBoxes = sorted(boundingBoxes, key=operator.itemgetter(1, 0)) # ordena os chars da esquerda para a direita e de cima para baixo
+    if __debug__:
+        for boundingBox in boundingBoxes:
+            x,y,w,h = boundingBox
+            roi = im_th[y:y+h, x:x+w] # the same, for a single character
+            cv2.rectangle(binary_image,(x,y),(x+w,y+h),(255,255,255),2)
+            #cv2.imshow("a", binary_image)
+            #cv2.waitKey(0)
+            #cv2.destroyAllWindows()
+    return characters, boundingBoxes
+
+def templateMatching(image, template):
+    result = cv2.matchTemplate(np.bitwise_not(image), template, cv2.TM_SQDIFF) # template matching
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result) # get values
+    #if __debug__:
+        #print(result)
+        #result = result*255
+        #cv2.imshow("Template matching result", result)
+    return min_val, max_val, min_loc, max_loc # return values
 
 def buildCharacterMap():
     alphabet = cv2.imread("alphabetALLCAPS.png")
     gray_alphabet = cv2.cvtColor(alphabet, cv2.COLOR_BGR2GRAY) #convert alphabet to grayscale
     th2, alphabet_th = cv2.threshold(gray_alphabet, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) #ensure binary
-    characters = findCharactersUsingContours(np.bitwise_not(alphabet_th))
+    characters, boundingBoxes = findCharactersUsingContours(np.bitwise_not(alphabet_th))
     charactermap = np.zeros((26,2,2))
-    for i, character in enumerate(characters):
-        x,y,w,h = cv2.boundingRect(character) #the same, for a single character
+    i=0;
+    for box in boundingBoxes:
+        x,y,w,h = box #the same, for a single character
         roi = alphabet_th[y:y+h, x:x+w] # the same, for a single character
-        # filename = "/charactermaps/"+str(chr(i+65))+".png"
-        # filename = str(filename)
-        # cv2.imwrite(os.path.join(filename), roi)
-        #cv2.imshow(str(i+65), roi) # show character
-        result = cv2.matchTemplate(np.bitwise_not(alphabet_th), roi, cv2.TM_SQDIFF) # template matching
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        min_val, max_val, min_loc, max_loc = templateMatching(alphabet_th, roi)
         charactermap[i][0] = max_loc
         charactermap[i][1] = min_loc
-    #print(charactermap)
+        i+=1
     return charactermap
 
 
 
 img = cv2.imread("testessss.png")
-alphabet = cv2.imread("alphabetALLCAPS.png")
+alphabet = cv2.imread("alfabeto2.png")
 threshold = 200
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert image to grayscale
 gray_alphabet = cv2.cvtColor(alphabet, cv2.COLOR_BGR2GRAY) #convert alphabet to grayscale
 th, im_th = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) #binary image
-#im_th_inv = np.bitwise_not(im_th) #invert binary
 th2, alphabet_th = cv2.threshold(gray_alphabet, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) #ensure binary
 
-
-#labeled_img = cc.findCharactersUsingConnectedComponents(im_th.copy()) #find blobs with connectedComponents
-#dft = dft.getDFT(im_th.copy()) #Get dft
-#im_floodfill = im_th_inv.copy() # invert image for floodfill
-#cv2.floodFill(im_floodfill, None, (0,0), 255); # find blobs with floodfill
-#im_floodfill = np.bitwise_not(im_floodfill) # invert image back to original
-
-#cv2.imshow("a", im_th)
 charactermap = buildCharacterMap()
-characters = findCharactersUsingContours(np.bitwise_not(im_th))
-
-#for i, ctr in enumerate(characters): # loop found characters
-     #x,y,w,h = cv2.boundingRect(ctr) # get bounds for each character
-     # Getting ROI
-     #roi = im_th[y:y+h, x:x+w] # get region of interest for each character
-     # show ROI
-     #cv2.imshow("segment", roi)
+#print(charactermap)
+characters, boundingBoxes = findCharactersUsingContours(np.bitwise_not(im_th))
 
 text = ""
 ranger = 5
-#character = findCharactersUsingContours(np.bitwise_not(im_th)) #find blobs with Contours
+
 img2 = im_th.copy()
 temp = 0
-for i, character in enumerate(characters):
-    x,y,w,h = cv2.boundingRect(character) #the same, for a single character
+
+for box in boundingBoxes:
+    x,y,w,h = box #the same, for a single character
     roi = im_th[y:y+h, x:x+w] # the same, for a single character
     cv2.rectangle(img2,(x,y),(x+w,y+h),(0,255,0),2)
     result = cv2.matchTemplate(np.bitwise_not(alphabet_th), roi, cv2.TM_SQDIFF) # template matching
@@ -80,26 +81,18 @@ for i, character in enumerate(characters):
     bottom_right = (top_left[0] + w, top_left[1] + h) #bottom_right of rectangle
     alphabet_highlight = alphabet_th.copy() # copy original image for highlight characters
     cv2.rectangle(alphabet_highlight, top_left, bottom_right, (0,0,0), 2)
-    # cv2.imshow("result", result)
-    # cv2.imshow("highlight", alphabet_highlight)
-    # cv2.imshow("asdlf", img2)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+
     if (temp-x) >= 29:
         text = text + " "
     #print(temp - x, temp, x)
     temp = x
+    i=0
     for i in range(len(charactermap)):
+        print(charactermap[i][0][0], max_loc[0], charactermap[i][0][1], max_loc[1], charactermap[i][0][0], max_loc[0]-ranger, charactermap[i][0][1], max_loc[1]-ranger, sep=", ")
         if charactermap[i][0][0] <= max_loc[0]+ranger and charactermap[i][0][1] <= max_loc[1]+ranger and charactermap[i][0][0] >= max_loc[0]-ranger and charactermap[i][0][1] >= max_loc[1]-ranger:
-            # print (charactermap[i][0][0], max_loc[0], sep = " - ")
-            # print (charactermap[i][0][1], max_loc[1], sep = " - ")
             text = text + str(chr(i+65))
-
-    #print(text)
+        i+=1
 print(text[::-1])
-#cv2.imshow('segment no:', img2) # show character
-#cv2.imshow("roi", alphabet_highlight)
-# cv2.imshow("img2", img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
